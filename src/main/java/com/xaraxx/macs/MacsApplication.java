5e44adb00b1ed97f9bac1570a623b6a557e8b587
sara galvan <sara.galvan@udea.edu.co>
2024-09-29 15:39:11 -0500

    creating first endpoint

---
+++ src/main/java/com/xaraxx/macs/MacsApplication.java
@@ -0,0 +1,13 @@
+package com.xaraxx.macs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacsApplication.class, args);
+	}
+
+}
