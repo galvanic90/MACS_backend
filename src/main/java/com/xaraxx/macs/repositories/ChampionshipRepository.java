3d20221f7a26df489ded60eeb30a47cda822e5e8
sara galvan <sara.galvan@udea.edu.co>
2024-10-29 01:21:52 -0500

    creating championship modelos 1

---
+++ src/main/java/com/xaraxx/macs/repositories/ChampionshipRepository.java
@@ -0,0 +1,8 @@
+package com.xaraxx.macs.repositories;
+
+import com.xaraxx.macs.models.Championship;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ChampionshipRepository extends CrudRepository<Championship, Integer> {
+
+}
