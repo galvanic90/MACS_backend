c3ffdc35fd923694b09bc5ad4d492253ee42a79a
sara galvan <sara.galvan@udea.edu.co>
2024-10-31 16:18:49 -0500

    creating categories by championship entity and migration

---
+++ src/main/java/com/xaraxx/macs/repositories/CategoriesByChampionshipRepository.java
@@ -0,0 +1,6 @@
+package com.xaraxx.macs.repositories;
+import com.xaraxx.macs.models.CategoriesByChampionship;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriesByChampionshipRepository extends CrudRepository<CategoriesByChampionship, Integer> {
+}
