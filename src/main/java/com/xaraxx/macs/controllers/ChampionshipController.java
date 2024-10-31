d5502f3f48dabe57ecedb6f6414441d3564f8a86
sara galvan <sara.galvan@udea.edu.co>
2024-10-31 07:48:35 -0500

    finishing champioship controller

--- src/main/java/com/xaraxx/macs/controllers/ChampionshipController.java
+++ src/main/java/com/xaraxx/macs/controllers/ChampionshipController.java
@@ -1,10 +1,9 @@
 package com.xaraxx.macs.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-
+import com.xaraxx.macs.exceptions.EntityNotFoundException;
 import com.xaraxx.macs.models.Championship;
 import com.xaraxx.macs.repositories.ChampionshipRepository;
-
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,6 +27,35 @@ public class ChampionshipController {
         return repository.findAll();
     }
 
-    
+    @PostMapping("/championship")
+    public Championship createChampionship(@RequestBody Championship newChampionship){
+        return repository.save(newChampionship);
+    }
+
+    @GetMapping("/championship/{id}")
+    public Championship getChampionshipById(@PathVariable Integer id){
+        return repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(id));
+    }
+
+    @PutMapping("/championship/{id}")
+    public Championship updateChampionshipById(@PathVariable Integer id, @RequestBody Championship newChampionship){
+        return repository.findById(id)
+                .map((championship)->{championship.setName(newChampionship.getName());
+                    championship.setClub(newChampionship.getClub());
+                    championship.setLocation(newChampionship.getLocation());
+                    championship.setStartEventDate(newChampionship.getStartEventDate());
+                    championship.setEndEventDate(newChampionship.getEndEventDate());
+                    return repository.save(championship);
+                 })
+                .orElseGet(() -> {
+                    return repository.save(newChampionship);
+                });
+    }
+
+    @DeleteMapping("/championship/{id}")
+    public void deleteChampionshipById(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
 
 }
