58ab97a03920ed3065714f6001691dd4d8706791
sara galvan <sara.galvan@udea.edu.co>
2024-10-31 19:33:33 -0500

    creating combats by champioship controller and creating delete method in registration controller

--- src/main/java/com/xaraxx/macs/controllers/RegistrationsByChampionshipController.java
+++ src/main/java/com/xaraxx/macs/controllers/RegistrationsByChampionshipController.java
@@ -21,23 +21,23 @@ public class RegistrationsByChampionshipController {
         this.repository = repository;
     }
 
-    @GetMapping("/registrations-champ")
+    @GetMapping("/registrations")
     public @ResponseBody Iterable<RegistrationsByChampionship> getAllRegistrationsByChampionship(){
         return repository.findAll();
     }
 
-    @PostMapping("/registrations-champ")
+    @PostMapping("/registrations")
     public RegistrationsByChampionship createRegistrationsByChampionship(@RequestBody RegistrationsByChampionship newRegistrations){
         return repository.save(newRegistrations);
     }
 
-    @GetMapping("/registrations-champ/{id}")
+    @GetMapping("/registrations/{id}")
     public RegistrationsByChampionship getRegistrationByChampionshipById(@PathVariable Integer id){
         return repository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(id));
     }
 
-    @PutMapping("/registrations-champ/{id}")
+    @PutMapping("/registrations/{id}")
     public RegistrationsByChampionship updateRegistrationByChampionshipById(@RequestBody RegistrationsByChampionship newRegistrations, @PathVariable Integer id){
         return repository.findById(id)
                 .map((registrations)->{registrations.setAthlete(newRegistrations.getAthlete());
@@ -48,4 +48,10 @@ public class RegistrationsByChampionshipController {
                     return repository.save(newRegistrations);
                 });
     }
+
+    @DeleteMapping("/registrations/{id}")
+    public void deleteCombatsByChampionshipById(@PathVariable Integer id){
+        repository.deleteById(id);
+    }
+
 }
