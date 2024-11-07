fb56557f79cf358d45f031b567fe7ff717e58be1
sara galvan <sara.galvan@udea.edu.co>
2024-11-07 00:53:11 -0500

    adding component anotation to categories mapper class

--- src/main/java/com/xaraxx/macs/mappers/CategoriesMapper.java
+++ src/main/java/com/xaraxx/macs/mappers/CategoriesMapper.java
@@ -7,7 +7,9 @@ import com.xaraxx.macs.repositories.CategoryAgeRepository;
 import com.xaraxx.macs.repositories.CategoryBeltGradeRepository;
 import com.xaraxx.macs.repositories.CategoryWeightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CategoriesMapper {
     @Autowired
     private final CategoryWeightRepository categoryWeightRepository;
